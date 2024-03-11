6dfb19b56fc4807778820f05d2d8fd52c8b11433
Sahar Rezaei <98565168+RezaeiSahar@users.noreply.github.com>
2024-03-10 19:51:48 -0500

    initial import

---
+++ Hello_word.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#SAHAR REZAEI
+#BIOL 5153
+
+
+# In[3]:
+
+
+get_ipython().system(' python --version')
+
+
+# In[5]:
+
+
+print("hello words")
+
+
+# In[ ]:
+
+
+
+
